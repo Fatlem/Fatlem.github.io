168bbf49dbc6398ab130b718d44154fdd7e06ae3
Fatlem YoY <122469156+Fatlem@users.noreply.github.com>
2024-06-06 16:22:19 +0700

    Add files via upload

---
+++ Nana/java/script.java
@@ -0,0 +1,23 @@
+// script.js
+function showGreeting() {
+    const name = document.getElementById('nameInput').value;
+    const greetingElement = document.getElementById('greeting');
+
+    if (name) {
+        greetingElement.textContent = `Selamat Ulang Tahun, ${name}! Semoga hari mu menyenangkan!`;
+    } else {
+        greetingElement.textContent = 'Tolong masukkan nama terlebih dahulu!';
+    }
+}
+
+document.getElementById('envelope').addEventListener('click', function() {
+    const cover = document.querySelector('.envelope-cover');
+    const letter = document.querySelector('.envelope-letter');
+    cover.style.top = '-50%';
+    letter.style.top = '100%';
+
+    setTimeout(() => {
+        document.getElementById('envelope').style.display = 'none';
+        document.getElementById('container').style.display = 'block';
+    }, 500);
+});
