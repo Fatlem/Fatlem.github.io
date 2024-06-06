45713b11e8dd1f429d0fd4d1e47b6e02a0503815
Fatlem YoY <122469156+Fatlem@users.noreply.github.com>
2024-06-06 16:35:20 +0700

    script.java

--- Nana/java/script.java
+++ Nana/java/script.java
@@ -4,7 +4,19 @@ function showGreeting() {
     const greetingElement = document.getElementById('greeting');
 
     if (name) {
-        greetingElement.textContent = `Selamat Ulang Tahun, ${name}! Semoga hari mu menyenangkan!`;
+        greetingElement.textContent = `Selamat Ulang Tahun, ${name}!  yang spesial untuk teman terbaikku! Hari ini adalah hari yang luar biasa karena kita merayakan keberadaanmu, sosok yang luar biasa dan penuh kebaikan. Dalam setiap langkah hidupmu, kamu telah menunjukkan kekuatan, kecantikan, dan ketulusan yang luar biasa.
+
+        Dalam cahayamu, aku menemukan inspirasi yang tak terbatas. Setiap kali kita bersama, kamu membawa sinar kebahagiaan yang mencerahkan hari-hariku. Kamu memiliki kekuatan untuk membuat orang-orang di sekitarmu tersenyum dan merasa diterima. Kamu adalah pilar kekuatan dan ketenangan, bahkan di tengah badai kehidupan.
+
+        Dengan segala kelembutan dan keanggunanmu, kamu memancarkan kecantikan yang sejati. Tapi lebih dari itu, kamu memiliki keindahan dalam hati yang tulus dan baik. Kamu selalu siap untuk mendengarkan, memberikan nasihat yang bijaksana, dan memberikan dukungan tanpa syarat. Kamu adalah teladan yang luar biasa tentang kebaikan dan kebaikan hati.
+
+        Sebagai teman, kamu tidak pernah ragu untuk menunjukkan dukunganmu. Kamu adalah teman yang setia dan tidak pernah kenal lelah. Dalam setiap langkah hidupmu, kamu telah menjadi inspirasi bagi banyak orang, termasuk aku. Aku bersyukur atas kehadiranmu dalam hidupku.
+
+        Hari ini adalah kesempatan bagi kita semua untuk merayakan keberadaanmu yang luar biasa. Kamu adalah sumber kegembiraan, kebahagiaan, dan kebanggaan bagi kita semua. Semoga setiap hari yang kamu lalui membawa berkah dan kebahagiaan yang tak terhitung jumlahnya. Semoga impian dan harapanmu terwujud dalam setiap langkah hidupmu.
+
+        Selamat Ulang Tahun, temanku tercinta! Terima kasih atas semua kebaikan, dan keanggunan yang kamu bagikan dengan dunia ini. Semoga tahun ini dan seterusnya membawa kesuksesan, kesehatan, dan kebahagiaan yang melimpah bagimu.
+
+        Teruslah bersinar, seperti bintang yang selalu menyinari langit malam. Kamu adalah cahaya dalam hidupku.`;
     } else {
         greetingElement.textContent = 'Tolong masukkan nama terlebih dahulu!';
     }
